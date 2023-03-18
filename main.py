18a7b5e1005ec93adac870d75524cb7aa97385ee
justinek <justinetaikoh@gmail.com>
2023-03-19 01:27:22 +0800

    Add implementation for setup init table function

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import xml.etree.ElementTree as ET
 from classes.Relation import Relation
 from classes.Query import Query, Task
 
+ALPHA = -1  # distinguished variable symbol
+
+
 def main(relation, query):
-    table = setUpInitTable(input)
+    # attrOrder is a list of attributes in the order as it appears in the table
+    attrOrder, table = setUpInitTable(relation, query)
     while not satisfyRequirement():
         data, changed = step(
             tableData, functionalDependencies, multiValuedDependencies)
@@ -14,9 +18,33 @@ def main(relation, query):
 
 
 # unique to each chase type
-def setUpInitTable(input):
+def setUpInitTable(relation, query):
     # convert the initTable to the tableData format
-    print("Hello World")
+    attrOrder = list(relation.attributes)
+    table = []
+
+    task = query.task
+    if task == Task.FUNCTIONAL_DEPENDENCY:
+        table = [[],[]]
+        lhs = query.to_check[0][0]
+        table[0] = [ALPHA for attr in attrOrder]
+        table[1] = [ALPHA if attr in lhs else attr + str(1) for attr in attrOrder]
+
+    elif task == Task.MULTIVALUED_DEPENDENCY:
+        table = [[],[]]
+        lhs = query.to_check[0][0]
+        rhs = query.to_check[0][1]
+        table[0] = [ALPHA if (attr in lhs or attr in rhs) else attr + str(0) for attr in attrOrder]
+        table[1] = [ALPHA if (attr in lhs or attr in set(attrOrder).difference(set(rhs))) else attr + str(1) for attr in attrOrder]
+
+    elif task == Task.LOSSLESS_JOIN:
+        n = len(query.to_check)  # number of subtables
+        table = [[] for i in range(0, n)]
+        for i in range(0, n):
+            schema = query.to_check[i].attributes
+            table[i] = [ALPHA if (attr in schema) else attr + str(i) for attr in attrOrder]
+
+    return (attrOrder, table)
 
 
 def handleOutput():
@@ -37,9 +65,10 @@ def satisfyRequirement():
     # if alr valid, return True
     return False
 
+
 def parse_input(input_file):
     """Takes in a xml file and returns 2 objects: `Relation` and `Query`
-    which represents the relational table and task that we are chaing
+    which represents the relational table and task that we are chasing
     respectively."""
 
     tree = ET.parse(input_file)
@@ -57,7 +86,7 @@ def parse_input(input_file):
                 relation.add_functional_dependency(lhs, rhs)
             elif child.tag == "multivalued_dependency":
                 relation.add_multivalued_dependency(lhs, rhs)
-    
+
     query = Query()
     for child in root.find("dependency_check"):
         if child.tag == "functional_dependency":
@@ -78,6 +107,7 @@ def parse_input(input_file):
             query.set_task(Task.LOSSLESS_JOIN)
             query.add_check(Relation(set(child.findall("attribute"))))
     return (relation, query)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(
